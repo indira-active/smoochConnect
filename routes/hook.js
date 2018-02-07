71a25b73854ed4eefb96ae9c63378d55aa5bf3dd
Andrew Williams <andrewwilliams1995@Andrews-MacBook-Pro.local>
2018-02-07 10:04:09 -0500

    reverting to regular log

--- routes/hook.js
+++ routes/hook.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req,res)=>{
-	console.log(`req.body is ${JSON.stringify(req.body)} and this is a get request on the hook route`)
+	console.log(`req.body is ${req.body} and this is a get request on the hook route`)
 	});
 
 router.post('/', (req,res)=>{
