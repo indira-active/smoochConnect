efa6ab16f477c57a3ae42e1a03c8f0515d581273
Andrew Williams <andrewwilliams1995@Andrews-MacBook-Pro.local>
2018-02-07 10:00:29 -0500

    stringifying the json in order to make it more readable on logs

--- routes/hook.js
+++ routes/hook.js
@@ -2,11 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req,res)=>{
-	console.log(`req.body is ${req.body} and this is a get request on the hook route`)
+	console.log(`req.body is ${JSON.stringify(req.body)} and this is a get request on the hook route`)
 	});
 
 router.post('/', (req,res)=>{
     console.log(`req.body is ${req.body} and this is a post request on the hook route`)
+    res.status(200);
+    res.send()
     });
 
 
