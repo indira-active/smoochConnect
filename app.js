1b555254ddf14a292b15922d7c1c51c4b1ccc354
Andrew Williams <andrewwilliams1995@Andrews-MacBook-Pro.local>
2018-02-11 10:42:39 -0500

    reverting back

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(cors());
 app.use('/api', api);
 app.use('/hook', api);
 
-app.use(express.static('./client/smooch-desk/build'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get("*",(req,res)=>{
   res.json({welcome:"to indira"})
 })
