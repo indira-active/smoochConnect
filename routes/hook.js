a3b576a253a62ad5990d5cc248cdc88f7d264cd2
Andrew Williams <andrewwilliams1995@Andrews-MacBook-Pro.local>
2018-02-11 15:04:59 -0500

    demo

--- routes/hook.js
+++ routes/hook.js
@@ -7,8 +7,9 @@ router.get('/', (req,res)=>{
 	});
 
 router.post('/', (req,res)=>{
-		io.to('testRoom').emit('testEvent', JSON.stringify(req.body,null,2))
-    console.log(`req.body is ${req.body} and this is a post request on the hook route`)
+		const msg = JSON.stringify(req.body,null,2);
+		io.to('testRoom').emit('testEvent', msg)
+    console.log(`req.body is ${msg} and this is a post request on the hook route`)
     res.status(200);
     res.send()
     });
