57b52822cc31da9a758a251fc24c43c5b76ef5c2
Andrew Williams <andrewwilliams1995@Andrews-MacBook-Pro.local>
2018-02-11 12:10:07 -0500

    testing webhooks with socket integration

--- client/smooch-desk/src/App.js
+++ client/smooch-desk/src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import io from 'socket.io-client';
-const socket = io();
+import io from 'socket.io-client'
+const socket = io('/');
 
 class App extends Component {
   componentWillMount = ()=>{
-      console.log('testing')
-      socket.on('testEvent', res=>{console.log(res);})
+    socket.on('testEvent', msg =>
+      {console.log(msg)}
+    );
   }
   render() {
-    return 
-      (<div className="App">
+    return (
+      <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Indira</h1>
         </header>
         <p className="App-intro">
-          messages below
+          messages
         </p>
       </div>
-    )
+    );
   }
 }
 
