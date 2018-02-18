885316080480b51c32fb97f2656f2f59583cb1c5
Andrew Williams <andrewwilliams1995@Andrews-MacBook-Pro.local>
2018-02-17 19:08:08 -0500

    test

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const jwt = require('jsonwebtoken');
-
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
 const KEY_ID = process.env.KEY_ID;
 const SECRET = process.env.SECRET;
