4b2ca5b74aafab8ba9b110b387c005205ef377a9
Andrew Williams <andrewwilliams1995@Andrews-MacBook-Pro.local>
2018-02-18 13:50:59 -0500

    test

--- client/src/App.js
+++ client/src/App.js
@@ -190,7 +190,7 @@ class App extends Component {
                                     onClick={() => {this.postDone(user._id)}}
                                     bsStyle="danger"
                                     bsSize="small">
-                                    TESTING2</Button>
+                                    BUILDOUT</Button>
                                 <span style={{position:"absolute",top:"-5px",left:"0px",backgroundColor:"black",color:"white",fontSize:"12px"}}>
                                 {user.unread || null}
                                 </span>
