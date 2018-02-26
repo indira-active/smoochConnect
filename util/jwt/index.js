41d3336b2a7f2955d415a5cd3a6af10b6a733efd
Andrew Williams <andrewwilliams1995@Andrews-MacBook-Pro.local>
2018-02-26 07:41:20 +0100

    test

--- util/jwt/index.js
+++ util/jwt/index.js
@@ -1,7 +1,11 @@
 const jwt = require('jsonwebtoken');
 const KEY_ID = process.env.KEY_ID;
 const SECRET = process.env.SECRET; 
-
+/** @function
+ * @name signJwt
+ * will use the SECRET and KEY_ID to create a unique jwt for the smooch instantiation
+ * first set of characters up until the first period is unique. rest of the characters are
+ * different per method call*/
 function signJwt(userId){
     const result = jwt.sign({
             scope: 'appUser',
